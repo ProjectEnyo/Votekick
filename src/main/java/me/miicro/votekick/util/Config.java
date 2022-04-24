8d6252ed35c121ede398f533ab63a262bb6d150f
indrer <indrer@users.noreply.github.com>
2022-04-24 21:37:11 +0200

    Add config
    Signed-off-by: indrer <indrer@users.noreply.github.com>

--- src/main/java/me/miicro/votekick/util/Config.java
+++ src/main/java/me/miicro/votekick/util/Config.java
@@ -1,4 +1,78 @@
 package me.miicro.votekick.util;
 
+import me.miicro.votekick.Votekick;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Config {
+
+    private Votekick vk;
+    private File file;
+    private FileConfiguration config;
+
+    public Config(Votekick vk) {
+        this.vk = vk;
+        reload();
+    }
+
+    public void reload() {
+        try {
+            file = new File(vk.getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                vk.saveResource("config.yml", false);
+            }
+            config = YamlConfiguration.loadConfiguration(file);
+        } catch(Exception e) {
+            vk.getLogger().info("There has been an issue trying to read config file. \n" + e.getMessage());
+        }
+    }
+
+    private void saveConfig() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            vk.getLogger().info("There has been an issue trying to save config file. \n" + e.getMessage());
+        }
+
+    }
+
+    public int getMinPlayers() {
+        if (config.get("min-players") == null) {
+            config.set("min-players", 2);
+            saveConfig();
+        }
+        return (int) config.get("min-players");
+    }
+
+    public int getVoteTime() {
+        if (config.get("vote-time") == null) {
+            config.set("vote-time", 30);
+            saveConfig();
+        }
+        return (int) config.get("vote-time");
+    }
+
+    public double getVotePercentage() {
+        if (config.get("needed-votes") == null) {
+            config.set("needed-votes", 0.5);
+            saveConfig();
+        }
+        double nVotes = (double) config.get("needed-votes");
+        if (nVotes > 1) {
+            nVotes = nVotes / 100;
+        }
+        return nVotes;
+    }
+
+    public String getKickMessage() {
+        if (config.get("kick-message") == null) {
+            config.set("kick-message", "You have been vote kicked off the server!");
+            saveConfig();
+        }
+        return (String) config.get("kick-message");
+    }
+
 }
